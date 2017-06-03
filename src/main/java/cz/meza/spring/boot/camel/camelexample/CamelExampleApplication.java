f147aade446b2a43254c0dd483167cf6eda987ea
Vladimir Mezera <vladimir.mezera@physter.com>
2017-06-03 15:53:02 +0200

    Add initial files with spring boot initialization

---
+++ src/main/java/cz/meza/spring/boot/camel/camelexample/CamelExampleApplication.java
@@ -0,0 +1,12 @@
+package cz.meza.spring.boot.camel.camelexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelExampleApplication.class, args);
+	}
+}
