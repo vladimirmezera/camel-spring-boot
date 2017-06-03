4bbeb2816768311ae49dada09830cd3c40975425
Vladimir Mezera <vladimir.mezera@physter.com>
2017-06-03 21:49:07 +0200

    Add basic test

--- src/test/java/cz/meza/spring/boot/camel/SpringBootCamelExampleTests.java
+++ src/test/java/cz/meza/spring/boot/camel/SpringBootCamelExampleTests.java
@@ -1,9 +1,14 @@
 package cz.meza.spring.boot.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.impl.DefaultExchange;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,12 +16,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootCamelExampleTests {
 
-	@Produce
-	private ProducerTemplate producerTemplate;
+	@Produce(uri = "direct:getInfo")
+	private ProducerTemplate producerGetInfo;
+
+	@Autowired
+	private CamelContext camelContext;
+
+	private static final String BASIC_INFORMATION = "Basic information";
 
 	@Test
-	public void contextLoads() {
-		System.out.println("d");
+	public void testBasicInformation() {
+		Exchange exchange = producerGetInfo.send(new DefaultExchange(camelContext));
+		Assert.assertEquals(BASIC_INFORMATION, exchange.getIn().getBody());
 	}
+
+	
 
 }
