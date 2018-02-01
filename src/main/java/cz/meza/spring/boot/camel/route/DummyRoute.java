6d4366ebfd4063b7d96757ed8499e5ee9cd4e0c9
vladimir.mezera <vladimir.mezera@gmail.com>
2018-02-01 22:41:08 +0100

    Fix test add componetn

--- src/main/java/cz/meza/spring/boot/camel/route/DummyRoute.java
+++ src/main/java/cz/meza/spring/boot/camel/route/DummyRoute.java
@@ -1,10 +1,12 @@
 package cz.meza.spring.boot.camel.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
 /**
  *  Dummy route
  */
+@Component
 public class DummyRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
